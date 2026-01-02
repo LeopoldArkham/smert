2f328e060be46aea6f02420a0a0a582b09827777
LeopoldArkham <leopold@drawbotics.com>
2026-01-02 20:59:18 +0100

    serve via anxum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+mod error;
+mod quote;
+
+use axum::{Router, http::StatusCode, response::Html, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/qotd", get(qotd));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:1212").await.unwrap();
+    let _ = axum::serve(listener, app).await; // Handle
 }
+
+async fn qotd() -> (StatusCode, Html<String>) {
+    let quote = quote::get_current_quote().unwrap();
+    (StatusCode::OK, Html(quote::render_quote(&quote).unwrap()))
+}
+
+// Restore my nice error handling with ?
